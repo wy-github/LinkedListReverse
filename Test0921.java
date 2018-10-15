2221755fad75f4ba270be4ccde93d211837109c0
wangyong <1721900182@qq.com>
2018-10-15 11:13:28 +0800

    first delete

--- Test0921.java
+++
@@ -1,8 +0,0 @@
-public class Test0921 {
-    public static void main(String[] args) {
-        String str1 = "hello";
-        String str2 = "ll";
-        boolean b = str1.startsWith(str2, 2);
-        System.out.println(b);
-    }
-}
